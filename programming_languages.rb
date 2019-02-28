335daa3e823381f5da457a1cd0708758212f4ed3
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-28 02:07:56 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
-def reformat_languages(languages)
+# def reformat_languages(languages)
   # your code here
-end
+# end
+
+require 'pry'
+
+def reformat_languages(languages)
+  # your code here	  new_hash = {} 
+  languages.each do |style, description|
+    description.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      end
+    end
+  end
+
+   new_hash  
+end	end
